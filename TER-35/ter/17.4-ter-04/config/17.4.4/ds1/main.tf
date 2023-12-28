2f6fc561726ce301c9080abf37ecc3e29ff50838
BaryshnikovNV <baryshnikov.nv@gmail.com>
2023-12-28 19:24:40 +0500

    Задание 17.5.3.2

--- TER-35/ter/17.4-ter-04/config/17.4.4/ds1/main.tf
+++ TER-35/ter/17.4-ter-04/config/17.4.4/ds1/main.tf
@@ -2,6 +2,11 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
+      version = "~>0.104.0"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "~>2.2.0"
     }
   }
   required_version = ">=0.13"
@@ -48,7 +53,7 @@ module "vpc_dev" {
 }
 
 module "test-vm" {
-  source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=95c286e"
   env_name        = "develop"
   network_id      = module.vpc_dev.network_id
   subnet_zones    = ["ru-central1-a"]
